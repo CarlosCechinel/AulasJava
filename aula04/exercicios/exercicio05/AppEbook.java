4afd86e331d9223839eee54e4cd5fa8236b6a3de
CarlosCechinel <cadu_cechinel@hotmail.com>
2021-03-20 18:25:52 -0300

    Nova versão Ebook

--- aula04/exercicios/exercicio05/AppEbook.java
+++ aula04/exercicios/exercicio05/AppEbook.java
@@ -2,16 +2,21 @@ package exercicios.exercicio05;
 
 public class AppEbook {
     public static void main(String[] args) {
-        Livro novo =new Livro("Como programar em java","Carlos", 2 );
+        Livro novo =new Livro("Como programar em Java.","Carlos", -1);
 
-        System.out.println("Pagina Atual; " + novo.exibirPagina());
+        System.out.println("Pagina Inicial: " + novo.exibirPagina());
 
         novo.avancaPagina();
-        System.out.println("Pagina atual: ");
 
         novo.avancaPagina();
 
         novo.retrocederPagina();
+
+        novo.irPag(2);
+
+        System.out.println("Exibindo página: "+ novo.exibirPagina());
+
+        novo.mostrarCapa();
 
     }
     
--- aula04/exercicios/exercicio05/Livro.java
+++ aula04/exercicios/exercicio05/Livro.java
@@ -7,34 +7,46 @@ public class Livro {
 public Livro(String titulo, String autor, int totalPaginas){ //pressuponto que comece na pagina 0 portanto nao comeca com pagina atual;
     this.titulo=titulo;
     this.autor=autor;
-    this.totalPaginas=totalPaginas;
+    verificaPag(totalPaginas);
+    this.paginaAtual=0;
+
 }
 public void avancaPagina(){
         paginaAtual=paginaAtual+1;
-        if (proximaPag>totalPaginas){
+        if (proximaPag>totalPaginas && paginaAtual<=0){
             System.out.println("Não é possivel avançar");
             paginaAtual=paginaAtual-1;
         }
-
+        System.out.println("Pagina atual: "+ paginaAtual);
     }
     void retrocederPagina(){ //ok
         paginaAtual=paginaAtual-1;
         if(pagAnterior<0){
             System.out.println("Não é possivel retroceder");
             pagAnterior=paginaAtual+1;
+            System.out.println("pagina atual: "+pagAnterior);
         }
+        System.out.println("Você esta na pagina: "+paginaAtual);
     }
-    void irPag(int totalPaginas, int iPag){
-        if (iPag>totalPaginas){
+    void irPag(int iPag){
+        if (iPag>totalPaginas && iPag <-1){
             System.out.println("Não foi possivel localizar a página");
 
         }
+        paginaAtual=iPag;
+        System.out.println("Voce foi para a pagina: "+paginaAtual);
     }
     public int exibirPagina(){ //ok
             return paginaAtual;
         }
-    public void mostrarCapa(int paginaAtual){
+    public void mostrarCapa(){
         paginaAtual=0;
-        System.out.println(titulo + ", autor: "+autor+ " paginas: "+totalPaginas);
+        System.out.println(titulo + "\nautor: "+autor+ "\npaginas: "+totalPaginas);
+    }
+    public void verificaPag(int totalPaginas2){
+        if (totalPaginas<=0){
+            System.out.println("Valor não aceito");
+        } 
+
     }
 }
