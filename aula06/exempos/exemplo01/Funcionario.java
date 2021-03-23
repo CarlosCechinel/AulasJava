28732d96d0a62742026c68afbe800d2b85b46570
CarlosCechinel <cadu_cechinel@hotmail.com>
2021-03-23 09:51:40 -0300

    Aula 06 - heranca

---
+++ aula06/exempos/exemplo01/Funcionario.java
@@ -0,0 +1,11 @@
+package exempos.exemplo01;
+
+public class Funcionario {
+    String nome;
+    private double salario;
+
+
+    public String exibir() {
+        return nome+ ": "+salario;
+    }    
+}
---
+++ aula06/exempos/exemplo01/Gerente.java
@@ -0,0 +1,5 @@
+package exempos.exemplo01;
+
+public class Gerente extends Funcionario { // extends puxa os atributos e metodos da classe funcionario
+    
+}
