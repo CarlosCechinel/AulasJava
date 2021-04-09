8d865a141f7e7cec004f45aa7187ca1a9fb16a78
CarlosCechinel <cadu_cechinel@hotmail.com>
2021-04-09 14:36:43 -0300

    Atualizar

--- Projeto/front/js/menu.js
+++ Projeto/front/js/menu.js
@@ -38,6 +38,24 @@ function gerarRelatorio() {
         .then(res => tratarResposta(res))
 }
 
+function relacaoAlarme() {
+    let msg = {
+        method: 'GET',
+        headers:{
+            'Content-type':'application/json'
+        }
+    }
+
+    fetch("http://localhost:8080/alarmes/all", msg)
+        .then(res => tratarResposta2(res))
+}
+
+function tratarResposta2(resposta) {
+    if(resposta){
+        resposta.json().then(res => exibirDados2(res))
+    }
+}
+
 function tratarResposta(resposta) {
     if(resposta){
         resposta.json().then(res => exibirDados(res))
@@ -51,6 +69,18 @@ function exibirDados(listaEventos) {
     for(i=0; i < listaEventos.length; i++) {
         let databr = new Date(listaEventos[i].dataevt).toLocaleDateString("pt-BR", {timeZone:'UTC'});
         tabela = tabela + `<tr> <td>${databr}</td>  <td>${listaEventos[i].alarme.nome}</td> <td>${listaEventos[i].equipamento.hostname}</td> </tr>`
+    }
+
+    tabela = tabela + '</table>'
+    document.getElementById("tabela").innerHTML = tabela
+}
+
+function exibirDados2(listaAlarmes) {
+    console.log(listaAlarmes);
+    let tabela = '<table class="table table-sm"> <tr> <th>ID</th> <th>Nome</th> <th>Descrição</th>  </tr>'
+
+    for(i=0; i < listaAlarmes.length; i++) {
+        tabela = tabela + `<tr> <td>${listaAlarmes[i].id}</td>  <td>${listaAlarmes[i].nome}</td> <td>${listaAlarmes[i].descricao}</td> </tr>`
     }
 
     tabela = tabela + '</table>'
